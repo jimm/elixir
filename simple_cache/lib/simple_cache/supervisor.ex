5faae1ef112348684b0071aee8ea6c9deff3d2d0
Jim Menard <jim@jimmenard.com>
2013-11-14 07:32:20 -0500

    removed debug output

--- simple_cache/lib/simple_cache/supervisor.ex
+++ simple_cache/lib/simple_cache/supervisor.ex
@@ -12,10 +12,7 @@ defmodule SimpleCache.Supervisor do
   end
 
   def init(_options) do
-    prototype = worker(SimpleCache.Element, [],
-                       restart: :temporary,
-                       shutdown: :brutal_kill)
-    IO.puts "prototype = #{inspect prototype}" # DEBUG
+    prototype = worker(SimpleCache.Element, [], restart: :temporary, shutdown: :brutal_kill)
     supervise([prototype], strategy: :simple_one_for_one, max_restarts: 0, max_seconds: 1)
   end
 end
