8b25ab15e49f1713a23ff28374b1c21efdabf57b
Jim Menard <jim@jimmenard.com>
2014-04-26 18:50:38 -0400

    Converts from YAML to Elixir Maps, using yamerl.

---
+++ yaml/yaml_to_map.ex
@@ -0,0 +1,36 @@
+defmodule YamlToMap do
+
+  @moduledoc """
+  Converts the output of :yamerl_constr (using the :detailed_constr option)
+  and turns it into a list of Elixir maps.
+  """
+
+  def to_maps(docs) when is_list(docs) do
+    docs |> Enum.map &_to_map(&1)
+  end
+
+  defp _to_map({:yamerl_doc, doc}), do: _to_map(doc)
+
+  defp _to_map({:yamerl_seq, :yamerl_node_seq, _tag, _loc, seq, _n}) do
+    seq |> Enum.map &_to_map(&1)
+  end
+
+  defp _to_map({:yamerl_map, :yamerl_node_map, _tag, _loc, map_tuples}) do
+    _tuples_to_map(map_tuples, %{})
+  end
+
+  defp _to_map({:yamerl_str, :yamerl_node_str, _tag, _loc, charlist}) do
+    String.from_char_data!(charlist)
+  end
+
+  defp _to_map({:yamerl_int, :yamerl_node_int, _tag, _loc, n}) do
+    n
+  end
+
+  defp _tuples_to_map([], map), do: map
+
+  defp _tuples_to_map([{key, val} | rest], map) do
+    {:yamerl_str, :yamerl_node_str, _tag, _log, name} = key
+    _tuples_to_map(rest, Dict.put_new(map, list_to_atom(name), _to_map(val)))
+  end
+end
---
+++ yaml/yaml_to_map_test.exs
@@ -0,0 +1,37 @@
+defmodule YamlToMapTest do
+  use ExUnit.Case
+
+  test "empty" do
+    assert convert('[]') == []
+    assert convert('{}') == %{}
+    assert convert('') == nil
+  end
+
+  test "get back a map with atomic keys" do
+    assert convert('a: b') == %{a: "b"}
+  end
+
+  test "more than one key" do
+    assert convert('{a: b, c: 42}') == %{a: "b", c: 42}
+  end
+
+  test "convert list values" do
+    assert convert('{a: [1, 2, 3]}') == %{a: [1, 2, 3]}
+    assert convert('{a: [1, [2, 3]]}') == %{a: [1, [2, 3]]}
+  end
+
+  test "convert lists of strings" do
+    assert convert('{a: [abc, def]}') == %{a: ["abc", "def"]}
+  end
+
+  test "recursive structures mapped" do
+    assert convert('a: {b: c}') == %{a: %{b: "c"}}
+  end
+
+  defp convert(s) do
+    yaml_doclist = s
+      |> :yamerl_constr.string(detailed_constr: true)
+    docs = YamlToMap.to_maps(yaml_doclist)
+    if docs == [], do: nil, else: hd(docs)
+  end
+end
