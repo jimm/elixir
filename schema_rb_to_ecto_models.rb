373e22057c9fc02007241989e80e66ab98db60ca
Jim Menard <jim@jimmenard.com>
2016-04-27 15:33:42 -0400

    don't include changeset comment

--- schema_rb_to_ecto_models.rb
+++ schema_rb_to_ecto_models.rb
@@ -146,12 +146,6 @@ EOS
   @required_fields ~w(#{required_fields.join(' ')})
   @optional_fields ~w(#{optional_fields.join(' ')})
 
-  @doc """
-  Creates a changeset based on the `model` and `params`.
-
-  If no params are provided, an invalid changeset is returned
-  with no validation performed.
-  """
   def changeset(model, params \\\\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
